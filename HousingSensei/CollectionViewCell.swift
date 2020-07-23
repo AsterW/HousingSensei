22b891bff5a3b66e6e214236cf1602c884734b94
HaolinWang <wanghaolin.andy@bytedance.com>
2020-07-23 18:31:06 +0800

    HousingSensei initial commit.

---
+++ HousingSensei/CollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CollectionViewCell.swift
+//  HousingSensei
+//
+//  Created by MonAster on 2019/12/1.
+//  Copyright © 2019 InvictusProgramming. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var cover: UIImageView!
+    //@IBOutlet weak var priceText: UILabel!
+    //@IBOutlet weak var coverText: UILabel!
+    @IBOutlet weak var priceText: UITextField!
+    
+    //@IBOutlet weak var coverText: UITextView!
+    @IBOutlet weak var coverText: UITextField!
+}
